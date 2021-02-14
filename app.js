0c8cc9cea84a5c29e6a963cd3490ee13c38c36a0
SCREENSABER <61020218+SCREENSABER@users.noreply.github.com>
2021-02-14 11:19:24 -0600

    ready for commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const double = (arr) => arr.map((val) => val * 2);
+
+const squareAndFindEvens = (numbers) => numbers.map((num) => num ** 2);
+
+const evens = squares.filter((square) => square % 2 === 0);
